1cc8fafd13410c478a85995acd72db86d4b5f261
mohitpradhan00 <pmohit0000@gmail.com>
2025-01-12 15:10:15 +0530

    .

---
+++ backend/myenv/ecommerce/ecomapp/products.py
@@ -0,0 +1,21 @@
+products = [
+    {
+        "_id":1,
+        "productname":"camera",
+        "category": "electronics",
+        "price":5000
+    },
+    {
+        "_id":2,
+        "productname":"laptop",
+        "category": "electronics",
+        "price":50000
+    },
+    {
+        "_id":3,
+        "productname":"mobile",
+        "category": "electronics",
+        "price":150000
+    },
+    
+]
